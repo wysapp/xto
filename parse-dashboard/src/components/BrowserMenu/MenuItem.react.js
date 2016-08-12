8ddfd35e7a6ec595232e53565eac5150add471f8
os x <osx@osdeiMac.local>
2016-08-12 11:15:32 +0800

    ys

---
+++ parse-dashboard/src/components/BrowserMenu/MenuItem.react.js
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+import styels from 'components/BrowserMenu/BrowserMenu.scss';
+
+
+let MenuItem = ({text, disabled, onClick}) => {
+  let classes = [styles.item];
+  if (disabled) {
+    classes.push(styles.disabled);
+  }
+
+  return <div className={classes.join(' ')} onClick={disabled ? undefined : onClick}>{text}</div>;
+};
+
+export default  MenuItem;
---
+++ parse-dashboard/src/dashboard/Data/Browser/SecurityDialog.react.js
@@ -0,0 +1,5 @@
+
+
+
+import Icon from 'components/Icon/Icon.react';
+import Parse from 'parse';
