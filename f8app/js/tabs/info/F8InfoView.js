e0ce20fcdb9b12152071c533ad24af35bb6cccdc
ys <345805844@qq.com>
2016-10-08 16:57:57 +0800

    ys

--- f8app/js/tabs/info/F8InfoView.js
+++ f8app/js/tabs/info/F8InfoView.js
@@ -24,7 +24,78 @@
  */
 'use strict';
 
+var CommonQuestions = require('./CommonQuestions');
+var LinksList = require('./LinksList');
 var ListContainer = require('ListContainer');
+var PureListView = require('../../common/PureListView');
 var React = require('React');
 var Relay = require('react-relay');
 var View = require('View');
+var WiFiDetails = require('./WiFiDetails');
+
+const POLICIES_LINKS = [{
+  title: 'Terms of Service',
+  url: 'https://m.facebook.com/terms?_rdr',
+}, {
+  title: 'Data Policy',
+  url: 'https://m.facebook.com/policies?_rdr',
+}, {
+  title: 'Code of Conduct',
+  url: 'https://www.fbf8.com/code-of-conduct',
+}];
+
+
+function F8InfoView() {
+  return (
+    <ListContainer
+      title="Information"
+      backgroundImage={require('./img/info-background.png')}
+      backgroundColor={'#47BFBF'}>
+      <InfoList />
+    </ListContainer>
+  );
+}
+
+function InfoList({viewer: {config, faqs, pages}, ...props}) {
+  return (
+    <PureListView
+      renderEmptyList={() => (
+        <View>
+          <WiFiDetails
+            network={config.wifiNetwork}
+            password={config.wifiPassword}
+          />
+          <CommonQuestions faqs={faqs} />
+          <LinksList title="Facebook pages" links={pages} />
+          <LinksList title="Facebook plicies" links={POLICIES_LINKS} />
+        </View>
+      )}
+      {...(props: any)}
+    />
+  );
+}
+
+
+InfoList = Relay.createContainer(InfoList, {
+  fragments: {
+    viewer: () => Relay.QL`
+      fragment on User {
+        config {
+          wifiNetwork
+          wifiPassword
+        }
+        faqs {
+          question
+          answer
+        }
+        pages {
+          title
+          url
+          logo
+        }
+      }
+    `,
+  },
+});
+
+module.exports = F8InfoView;
